9d626aea7b217af468b2d1f62228a382825a7a2f
gshahg <imranabbas67@gmail.com>
2018-10-10 02:23:08 -0700

    Videos on Feed

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -3,33 +3,15 @@ const path = require("path");
 
 //to give the storage location and file name;
 const storage = multer.diskStorage({
-    destination: '../videos/',
+    destination: '../client/src/assets/',
     filename: (req, file, callback) => {
         callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
 
-// //to validate the file is in vallide "jpeg|jpg|png" format
-// const checkFileType = (file, callback) => {
-// 	//Allowed Ext
-// 	const fileTypes = /jpeg|jpg|png/;
-// 	//check Ext
-// 	const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
-// 	//check mimeType
-// 	const mimetype = fileTypes.test(file.mimetype);
-//     if (extName && mimetype){
-//         callback(null, true);
-//     } else {
-//         callback("Error: Image Only..!!!");
-//     }
-// }
-
 const uploadProductImage = multer({
     storage: storage,
     limits: {fileSize: 20971520},//20MB file size limit
-	// fileFilter: (req,file,callback) => {
-	// 	checkFileType(file,callback);
-	// }
 }).single('file');
 
 
